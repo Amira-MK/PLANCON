92631fa46162684b46a761f06fab057218b5b9d1
Amira-Mk <mekkiamira85@gmail.com>
2022-03-19 00:14:57 +0100

    Authentif

--- sign.js
+++
@@ -1,8 +0,0 @@
-function change(){
-    var forms = document.querySelectorAll('.form')
-    for (let i = 0; i< forms.length; i++) {
-        var form = forms[i];
-        if(form.classList.contains('hide')) {form.classList.remove('hide')}
-        else form.classList.add('hide')
-      }
-}
